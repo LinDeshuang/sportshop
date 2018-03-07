9d796b34ce745e038ed94d735b12c3cbe476dae5
LinDeshuang <1287702249@qq.com>
2018-03-07 22:07:34 +0800

    tetest

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php 
-	require_once('/include/public.php');
-	require_once('/public/class/db.php');
-	require_once('/public/class/function.php');
+	require_once('include/public.php');
+	require_once('public/class/db.php');
+	require_once('public/class/function.php');
  ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>体育用品商城</title>
 	<?php
-		include_once('/include/head.php');
+		include_once('include/head.php');
 	?>
 </head>
 <body>
 	<?php
-		include_once('/include/header.php');
+		include_once('include/header.php');
 	?>
 	<div class="search-bar">
 		<div class="layui-container">
@@ -153,11 +153,11 @@
 	</div>
 
 	<?php
-		include_once('/include/footer.php');
+		include_once('include/footer.php');
 	?>
 </body>
 	<?php
-		include_once('/include/script.php');
+		include_once('include/script.php');
 	?>
 	<script>
 		layui.use('carousel', function(){
